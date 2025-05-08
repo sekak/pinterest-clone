9fb2c777045f414754d7d08bf63e38f880cba787
sekak <ahmadesekak@gmail.com>
2025-05-09 00:21:46 +0100

    Increase z-index of TopBar for improved visibility

--- client/src/components/topBar/topBar.tsx
+++ client/src/components/topBar/topBar.tsx
@@ -13,7 +13,7 @@ export default function TopBar() {
   }
 
   return (
-    <div className='flex items-center w-full p-3 sticky top-0 bg-white z-10'>
+    <div className='flex items-center w-full p-3 sticky top-0 bg-white z-50'>
       <form onSubmit={handleSearch} className='flex items-center bg-gray-200 rounded-xl flex-1 px-4 py-1.5 my-1 gap-1'>
         <SearchIcon className='text-gray-600' />
         <input type="search" placeholder="Search" className="border p-1.5 border-none outline-none w-full text-gray-700 text-[16px]" />
